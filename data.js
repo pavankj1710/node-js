c51196d1291a1ff6878d3d3e25e40ce318b09c06
pavankj1710 <jamadarpavankumar59@gmail.com>
2022-09-23 10:46:47 +0530

    sample

--- data.js
+++
@@ -1,6 +0,0 @@
-const data=[
-    {name:'Pavanku mar',email:'pavanQgmail.com',contact:'9987'},
-    {name:'Navan',email:'navanQgmail.com'},
-    {name:'jamadar',email:'jamadarpavanQgmail.com'}
-]
-module.exports=data;
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-//const colors = require('colors');
-//console.log("packagejson".red);
-// console.warn(" nodemon successfully am verry happpy")
-// console.warn(100+20);
-const http = require('http');
-const data=require('./data');
-http.createServer((req,resp)=>{
-resp.writeHead(200,{'Content-Type':'application\json'});
-resp.write(JSON.stringify(data ));
-resp.end();
-}).listen(6008);
+//console.log(process.argv)
+
+// CREAT FILE
+// const fs = require('fs');
+// const input = process.argv;
+// fs.writeFileSync(input[2],input[3])
+
+// REMOVE AND ADD FILE
+const fs = require('fs');
+const input = process.argv;
+
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4])
+}else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3])
+}else{
+    console.log("invalid input")
+}
